4c85f81d53a387742237542b9abd74279a8e21d6
Al-Mahfuzur Rahman <144318749+tusar431@users.noreply.github.com>
2023-10-22 13:33:53 +0600

    Add files via upload

---
+++ jSexam1.js
@@ -0,0 +1,205 @@
+// Ans to the ques no 5
+// var name = "Tusar";
+
+// Ans To the ques no 3
+// data type      example
+// string         "Text"
+// intiger         12,15,4
+// float           12.34
+
+// ans to the ques no 16
+// Ans is 0
+
+// Ans to the ques no 13
+// var x = 30;
+// var y = '23';
+
+// var sum = x+y;
+
+// Ans to the ques no 25
+// var totalCostofpen = 185;
+// var quantityOfCostpen = 10;
+// var quantityOfbuyingpen = 3;
+
+// var costofOnePen = totalCostofpen/quantityOfCostpen;
+// var priceOfthreePen = costofOnePen*quantityOfbuyingpen;
+
+// Ans to the ques no 2
+// 1st use the keyword var
+// then type veriable name
+// after typing variable name
+// and then use = to defain veriable value 
+// and use ; to complete the statement
+
+// Ans to the Ques no 4
+// 1- Name sobsomoy text diye suru hobe
+// 2- Number diye variable name suru kora jabe na
+// 3- Keyword ke variable name hisabe use kora jabe na
+// 4- variable er nam obossoi signle word hote hobe double word houya jabe na
+// 5- Variabe er name a bivinno text case use kora jay like camle case
+
+// Ans to the ques no6
+// Arithmetic Operator dara bivinno ganitik kaj jemon jog,biyog,gun,vag ittadi kora jay
+
+// Ans to the ques no 8
+// + hosse ekti operator. ei operator j guloke niye kaj kore tai hosse oparend.example
+// 1+2 ei khaen 1 o 2 hosse operend r + hosse operator
+
+// Ans to the Ques No 9
+// var x =10;
+// var y =36;
+// var z =(x+y);
+// console.log(z);
+
+// Ans to the ques no 10 
+// var num1 = 8;
+// var num2 = 375;
+// var Vagses=(num2%num1);
+// console.log(Vagses);
+
+// Ans to the Ques No 11
+// var num1 =8;
+// var num2 = 3;
+// var Num3 =(num1**num2);
+// console.log(Num3);
+
+// Ans to the qus no 12
+// var Math = 1;
+// var Chemistry = 1;
+// var physics = 1;
+// var bangla = 1;
+// var totalSub =4;
+// var Total=(Math+Chemistry+physics+bangla);
+// var avarage=Total/totalSub;
+// console.log(avarage);
+
+// Ans to the Ques no 14
+// typeof() diye value ta kun type er ta dekha jay
+// example
+// var number = 'hii';
+// console.log(typeof(number));
+
+// Ans to the ques no 15
+// var thisISastring = '143';
+// var convert = parseInt(thisISastring);
+// console.log(convert);
+
+// Ans to the ques no 17
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers[5]=334;
+// console.log(numbers);
+
+// Ans to the ques no 18
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// numbers.push(234);
+// var way2 = numbers.unshift(998);
+// console.log(numbers);
+
+// Ans to the ques no 22
+// var fruits = ["Banana","Orange","Apple","Mango"];
+// var newfruits = fruits.splice(3,0,"Lichi","Pineanapple","Watermelon");
+// console.log(newfruits);
+
+// ans to the ques nop 20
+// var team1 =["rahim","karim"];
+// var team2 =["tanim","biplob","tarek"];
+// var team3 =["sajjad","arefin"];
+// var newteam= team1.concat(team2,team3);
+// console.log(newteam);
+
+// Ans to the Ques no 19
+// var numbers = [[1,2],[3,4],[5,6]];
+// var newnum =numbers.join();
+// console.log(newnum);
+
+// Ans to the ques no 21
+// var fruits = ["Banana","Orange","Apple","Mango"];
+// fruits.shift(0);
+// console.log(fruits);
+
+// Ans to the ques no 23
+// var x=10;
+// var y=15;
+// if(x>y){console.log("X is larger than y");
+// }else{
+//     console.log("Y is larger then x");
+// }
+
+// Ans to the ques no 26
+// var num1 = 90;
+// var num2 = 23;
+// var percentage =(num1*num2)/100;
+// console.log(percentage);
+
+// Ans to the ques no27 
+// var fruits = ["Banana","Orange","Apple","Mango"];
+// var result = fruits.indexOf("Kiwi");
+// console.log(result);
+
+// // Ans to the ques no 7
+// % = Modulas( eti diye vagses ber kora hoy)
+// ** = Exponentation (eti diye scoyar ber kora hoy)
+// ++ = Increment(eti babohar korle value 1 bere jabe)
+// -- = decrement(eti use kore 1 kore komano jay)
+
+// Ans to the ques no 1
+// var hi="This is a Statement"
+
+// Ans to the ques no 29
+// var Bangla = 50;
+// var English= 5;
+// var Math = 60;
+// var Ict = 50;
+// var totalSubject=4;
+// var total =(Bangla+English+Math+Ict);
+// var mark= total/totalSubject;
+// if(mark >=90 & mark<=100)
+// {
+//     console.log("S Garde");
+// }else if(mark >=80 & mark<=89)
+// {
+//     console.log("A Grade");
+// }else if(mark >= 70 & mark<=79)
+// {
+//     console.log("B Garde")
+// }
+// else if(mark >= 60 & mark<=69)
+// {
+//     console.log("c Garde")
+// }
+// else if(mark >= 50 & mark<=59)
+// {
+//     console.log("D Garde")
+// }
+// else if(mark >= 40 & mark<=49)
+// {
+//     console.log("E Garde")
+// }
+// else
+// {
+//     console.log("Student Has Failed")
+// }
+
+// Ans to the ques no 28 
+// var Sinagara=20;
+// var Somucha=15;
+// var bonRuti=10;
+// var Burger=60;
+// var Budget =5;
+
+// if(Budget>= Burger){
+//     console.log("Available Burger,Singara,Somucha,BonRuti")
+// }else if(Budget>=Sinagara){
+//     console.log("Available Singara,Somucha,BonRuti")
+// }
+// else if(Budget>=Somucha){
+//     console.log("Available Somucha,BonRuti")
+// }
+// else if(Budget>=bonRuti){
+//     console.log("Available BonRuti")
+// }else{
+//     console.log("Sorry you are very poor to eat")
+// }
+
+// Ans To the ques no 30
+// I think ami parbo.ami amar serata diye try kori korar jonne.Insa allah ami valo kisu korte parbo.
